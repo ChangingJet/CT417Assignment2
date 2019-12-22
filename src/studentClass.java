dfb05ccf1636801d12ee16a786e0dbc70a661b81
ChangingJet <M.Moore14@nuigalway.ie>
2019-12-22 17:02:12 +0000

    New Student class

--- src/studentClass.java
+++ src/studentClass.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
 
 public class studentClass {
 	
-	public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
-    } 
+	public static String name = "Matthew";
+	public static int age = 22;
+	public static String DOB = "1/12/1997";
+	public static int ID = 16497746;
+	public static String username = name+age;
+	public static String course = "Electronic + Computer Engineering";
+	public static String modules = "CT417";
 	
-}
+	public static void main(String args[]){  
+		String student = "[ "+name+", "+age+", "+DOB+", "+ID+", "+username+", "+course+", "+modules+" ]";
+		System.out.println(student);
+	}  
+}  
+
